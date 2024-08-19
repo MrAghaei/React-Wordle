37ab40af48be4d120b81fa086cd319e9ebe9ceb1
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-08-19 20:20:27 +0330

    #1 implement database (#1)
    * [#1-ADD] database file
    
    * [#1-Implement] data fetching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json=> {
+      console.log(json)
+      const randomSolution = json[Math.floor(Math.random()*json.length)]
+      setSolution(randomSolution.word)
+    })
+
+  },[setSolution])
+
   return (
     <div className="App">
       <h1>Wordle (Lingo)</h1>
+      {solution && <div>This is the solution: {solution}</div>}
     </div>
   );
 }
